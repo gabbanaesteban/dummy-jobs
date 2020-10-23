039788510a3c3a26da93c073844b41e55f687113
Esteban De la Rosa <gabbanaesteban@gmail.com>
2020-10-23 00:45:15 -0400

    Use mongo_uri env var

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,29 +5,10 @@ import mongoose from 'mongoose'
 
 configDotenv()
 
-const {
-  MONGO_HOST,
-  MONGO_PORT,
-  MONGO_USER,
-  MONGO_PASSWORD,
-  MONGO_DATABASE_NAME
-} = process.env
+const { MONGO_URI } = process.env
 
-export function _getDefaultUri(): string {
-  const host = MONGO_HOST || 'localhost'
-  const port = MONGO_PORT || '27017'
-  const user = MONGO_USER || ''
-  const password = MONGO_PASSWORD || ''
-
-  if (!user || !password) {
-    return `mongodb://${host}:${port}/${MONGO_DATABASE_NAME}`
-  }
-
-  return `mongodb://${user}:${password}@${host}:${port}/${MONGO_DATABASE_NAME}?authSource=admin`
-}
-
-export function connect(uri = _getDefaultUri(), options = {}) {
-  // eslint-disable-next-line no-undef
+  // @ts-ignore
+export function connect(uri: string = MONGO_URI, options = {}) {
   return mongoose.connect(
     uri,
     {...options, useNewUrlParser: true }
